3c935f7a7ece73bcb830dfdbba6c20f29c5b018d
jamesmorley45 <jamesmorley6845@gmail.com>
2024-12-15 11:01:20 +0500

    added

---
+++ script.js
@@ -0,0 +1,194 @@
+let box1 = document.querySelector('.box1-input');
+let box2 = document.querySelector('.box2-input');
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+let btn3 = document.querySelector('.btn3');
+let btn4 = document.querySelector('.btn4');
+let btn5 = document.querySelector('.btn5');
+let btn6 = document.querySelector('.btn6');
+let btn7 = document.querySelector('.btn7');
+let btn8 = document.querySelector('.btn8');
+let btn9 = document.querySelector('.btn9');
+let btn10 = document.querySelector('.btn10');
+let btn11 = document.querySelector('.btn11');
+let btn12 = document.querySelector('.btn12');
+    
+btn1.addEventListener('click', () => {
+    box1.classList.add('col1');
+    box1.classList.remove('col2');
+    box1.classList.remove('col3');
+    box1.classList.remove('col4');
+    box1.classList.remove('col5');
+    box1.classList.remove('col6');
+    box1.classList.remove('col7');
+    box1.classList.remove('col8');
+    box1.classList.remove('col9');
+    box1.classList.remove('col10');
+    box1.classList.remove('col11');
+    box1.classList.remove('col12');
+});
+
+btn2.addEventListener('click', () => {
+    box1.classList.add('col2');
+    box1.classList.remove('col1');
+    box1.classList.remove('col3');
+    box1.classList.remove('col4');
+    box1.classList.remove('col5');
+    box1.classList.remove('col6');
+    box1.classList.remove('col7');
+    box1.classList.remove('col8');
+    box1.classList.remove('col9');
+    box1.classList.remove('col10');
+    box1.classList.remove('col11');
+    box1.classList.remove('col12');
+});
+
+btn3.addEventListener('click', () => {
+    box1.classList.add('col3');
+    box1.classList.remove('col1');
+    box1.classList.remove('col2');
+    box1.classList.remove('col4');
+    box1.classList.remove('col5');
+    box1.classList.remove('col6');
+    box1.classList.remove('col7');
+    box1.classList.remove('col8');
+    box1.classList.remove('col9');
+    box1.classList.remove('col10');
+    box1.classList.remove('col11');
+    box1.classList.remove('col12');
+});
+
+btn4.addEventListener('click', () => {
+    box1.classList.add('col4');
+    box1.classList.remove('col1');
+    box1.classList.remove('col3');
+    box1.classList.remove('col2');
+    box1.classList.remove('col5');
+    box1.classList.remove('col6');
+    box1.classList.remove('col7');
+    box1.classList.remove('col8');
+    box1.classList.remove('col9');
+    box1.classList.remove('col10');
+    box1.classList.remove('col11');
+    box1.classList.remove('col12');
+});
+
+btn5.addEventListener('click', () => {
+    box1.classList.add('col5');
+    box1.classList.remove('col1');
+    box1.classList.remove('col3');
+    box1.classList.remove('col4');
+    box1.classList.remove('col2');
+    box1.classList.remove('col6');
+    box1.classList.remove('col7');
+    box1.classList.remove('col8');
+    box1.classList.remove('col9');
+    box1.classList.remove('col10');
+    box1.classList.remove('col11');
+    box1.classList.remove('col12');
+});
+
+btn6.addEventListener('click', () => {
+    box1.classList.add('col6');
+    box1.classList.remove('col1');
+    box1.classList.remove('col3');
+    box1.classList.remove('col4');
+    box1.classList.remove('col5');
+    box1.classList.remove('col2');
+    box1.classList.remove('col7');
+    box1.classList.remove('col8');
+    box1.classList.remove('col9');
+    box1.classList.remove('col10');
+    box1.classList.remove('col11');
+    box1.classList.remove('col12');
+});
+
+btn7.addEventListener('click', () => {
+    box2.classList.add('col7');
+    box2.classList.remove('col1');
+    box2.classList.remove('col3');
+    box2.classList.remove('col4');
+    box2.classList.remove('col5');
+    box2.classList.remove('col6');
+    box2.classList.remove('col2');
+    box2.classList.remove('col8');
+    box2.classList.remove('col9');
+    box2.classList.remove('col10');
+    box2.classList.remove('col11');
+    box2.classList.remove('col12');
+});
+
+btn8.addEventListener('click', () => {
+    box2.classList.add('col8');
+    box2.classList.remove('col1');
+    box2.classList.remove('col3');
+    box2.classList.remove('col4');
+    box2.classList.remove('col5');
+    box2.classList.remove('col6');
+    box2.classList.remove('col2');
+    box2.classList.remove('col7');
+    box2.classList.remove('col9');
+    box2.classList.remove('col10');
+    box2.classList.remove('col11');
+    box2.classList.remove('col12');
+});
+
+btn9.addEventListener('click', () => {
+    box2.classList.add('col9');
+    box2.classList.remove('col1');
+    box2.classList.remove('col3');
+    box2.classList.remove('col4');
+    box2.classList.remove('col5');
+    box2.classList.remove('col6');
+    box2.classList.remove('col2');
+    box2.classList.remove('col8');
+    box2.classList.remove('col7');
+    box2.classList.remove('col10');
+    box2.classList.remove('col11');
+    box2.classList.remove('col12');
+});
+
+btn10.addEventListener('click', () => {
+    box2.classList.add('col10');
+    box2.classList.remove('col1');
+    box2.classList.remove('col3');
+    box2.classList.remove('col4');
+    box2.classList.remove('col5');
+    box2.classList.remove('col6');
+    box2.classList.remove('col2');
+    box2.classList.remove('col8');
+    box2.classList.remove('col9');
+    box2.classList.remove('col7');
+    box2.classList.remove('col11');
+    box2.classList.remove('col12');
+});
+
+btn11.addEventListener('click', () => {
+    box2.classList.add('col11');
+    box2.classList.remove('col1');
+    box2.classList.remove('col3');
+    box2.classList.remove('col4');
+    box2.classList.remove('col5');
+    box2.classList.remove('col6');
+    box2.classList.remove('col2');
+    box2.classList.remove('col8');
+    box2.classList.remove('col9');
+    box2.classList.remove('col10');
+    box2.classList.remove('col7');
+    box2.classList.remove('col12');
+});
+
+btn12.addEventListener('click', () => {
+    box2.classList.add('col12');
+    box2.classList.remove('col1');
+    box2.classList.remove('col3');
+    box2.classList.remove('col4');
+    box2.classList.remove('col5');
+    box2.classList.remove('col6');
+    box2.classList.remove('col2');
+    box2.classList.remove('col8');
+    box2.classList.remove('col9');
+    box2.classList.remove('col10');
+    box2.classList.remove('col11');
+    box2.classList.remove('col7');
+});
